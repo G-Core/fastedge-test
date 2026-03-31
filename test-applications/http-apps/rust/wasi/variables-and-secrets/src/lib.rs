8c3a72aed448733c027c1ec1c1d4278304ff0cbf
Gordon Farquharson <godronus@yahoo.com>
2026-03-31 11:00:50 +0100

    remove bad comment

--- test-applications/http-apps/rust/wasi/variables-and-secrets/src/lib.rs
+++ test-applications/http-apps/rust/wasi/variables-and-secrets/src/lib.rs
@@ -3,9 +3,6 @@ use wstd::http::body::Body;
 use wstd::http::{Request, Response};
 use fastedge::secret;
 
-/// Note: wstd does not have a secret API. Both USERNAME and PASSWORD
-/// are read as env vars. The test fixture .env must set both as
-/// FASTEDGE_VAR_ENV_ prefixed variables.
 #[wstd::http_server]
 async fn main(_request: Request<Body>) -> anyhow::Result<Response<Body>> {
     let username = env::var("USERNAME").unwrap_or_default();
